24bddcb6016dc9aa7c866c43150e2d3c5cfa4bb1
uluagac <ersinuluagac@gmail.com>
2025-08-22 22:02:56 +0300

    program.cs basic settings

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
